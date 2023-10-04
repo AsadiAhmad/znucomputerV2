6a8da33a6bbe58e8cfb9ce6f4372ce7adc1eb908
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2023-10-04 23:35:44 +0330

    add SQL change connection

--- PHP/connection.php
+++ PHP/connection.php
@@ -1,6 +1,6 @@
 <?php
-  $servername = "znucomputer.ir";
-  $username = "znucompu";
+  $servername = "linux23";
+  $username = "znucompu_znucomputer";
   $password = "Ah-As-22-ij";
   $dbname = "znucompu_znu";
 
@@ -9,4 +9,17 @@
     die("could not connect".mysqli_connect_error());
   else
     echo "Connection established";
+
+  $sql = "INSERT INTO `links` (`Semster`, `Lesson`, `Section`, `Date`, `Link`) VALUES
+  (6, 'AI', 1, '2023-10-01', 'http://google.com'),
+  (6, 'AI', 2, '2023-10-02', 'http://google.com');";
+  
+    
+  if (mysqli_query($connction, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($connction);
+  }
+    
+  mysqli_close($connction);
 ?>
---
+++ SQL/first.sql
@@ -0,0 +1,4 @@
+DROP TABLE links;
+INSERT INTO `links` (`Semster`, `Lesson`, `Section`, `Date`, `Link`) VALUES
+(6, 'AI', 1, '2023-10-01', 'http://google.com'),
+(6, 'AI', 2, '2023-10-02', 'http://google.com');
